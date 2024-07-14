40b5389234c475b0eefef8b1e22edbf6461d4f66
JaimePRO200421212121 <165422995+JaimePRO200421212121@users.noreply.github.com>
2024-07-14 16:33:02 +0200

    Created Functions.lua

---
+++ Functions.lua
@@ -0,0 +1,11 @@
+local Functions = {};
+
+Functions.Variables = {
+    ["NoClipping"] = nil,
+    ["ESPEnabled"] = false,
+    ["Flying"] = false,
+    ["QEFly"] = true,
+    ["FlySpeed"] = 1
+};
+
+return Functions;
